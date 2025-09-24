6fd593221041f86a4097d5d91dd7508796269d91
Pradeep kumar Sahu <109669690+psahugm@users.noreply.github.com>
2025-09-24 08:57:41 +0530

    Debug changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SharedModule } from '../app/shared/shared.module';
+import { BreadcrumbComponent } from '../app/shared/components/breadcrumb.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet, SharedModule],
+  imports: [RouterOutlet, SharedModule, BreadcrumbComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
