21dfa424acba54345b39288bfdd71000b45bd94c
Dhabaleswar <dhabaleswar_prusty@yahoo.co.in>
2026-02-16 05:17:54 +0530

    Added code to plan event

--- src/app/core/models/event.ts
+++ src/app/core/models/event.ts
@@ -4,9 +4,18 @@ export interface EventItem {
   OrgLevelID?: number;
   DistrictID?: number;
   SamithiID?: number;
-  EventTitle: string;
-  EventVenue: string;
-  EventDescription: string;
-  EventDate: string;
-  EventAgendaImageUrl: string;
+  EventTitle?: string;
+  EventVenue?: string;
+  EventDescription?: string;
+  EventDate?: string;
+  IsToBeFeatured?: boolean;
+  IsActive?: boolean;
+  IsDeleted?: boolean;
+  IsApproved?: boolean;
+  RejectionComments?: string;
+  EventBannerImageUrl?: string;
+  EventAgendaImage1Url?: string;
+  EventAgendaImage2Url?: string;
+  EventAttendees?: string;
+  KeyTakeaways?: string;
 }
