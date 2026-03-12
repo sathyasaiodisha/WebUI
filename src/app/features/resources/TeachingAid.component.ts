a780eef5365ea3c4787d1564ce0e772791be7b0b
Pradeep kumar Sahu <109669690+psahugm@users.noreply.github.com>
2026-03-12 10:33:57 +0530

    stotras v1

--- src/app/features/resources/TeachingAid.component.ts
+++ src/app/features/resources/TeachingAid.component.ts
@@ -13,48 +13,52 @@ import { MatExpansionModule } from '@angular/material/expansion';
 
         <table class="table table-bordered">
           <tbody>
-            @for(ro of data; track $index){
-            <tr>
-              <td>
-                {{ ro?.title ?? ro.link }}
-              </td>
-              <td>
-                <a
-                  id="ContentPlaceHolder1_HyperLink1"
-                  target="_blank"
-                  href="{{ ro.link }}"
-                  >View</a
-                >
-              </td>
-            </tr>
+            @for (ro of data; track $index) {
+              <tr>
+                <td>
+                  {{ ro?.title ?? ro.link }}
+                </td>
+                <td>
+                  <a
+                    id="ContentPlaceHolder1_HyperLink1"
+                    target="_blank"
+                    href="{{ ro.link }}"
+                    >View</a
+                  >
+                </td>
+              </tr>
             }
           </tbody>
         </table>
-        <!--
-        <mat-accordion multi>
-          @for(cat of data;track $index; let i = $index){
-
+        <h1 class="h1 text-darkblue ff-source-serif-semi-bold border-heading">
+          {{ 'stotras' | translate }}
+        </h1>
+        <ng-container
+          *ngTemplateOutlet="listTemplate; context: { list: data_stotras }"
+        ></ng-container>
+      </div>
+    </div>
+    <ng-template #listTemplate let-list="list">
+      <mat-accordion>
+        @for (cat of list; track $index; let i = $index) {
+          @if (cat.links?.length > 0) {
             <mat-expansion-panel [expanded]="i === 0">
               <mat-expansion-panel-header>
                 <mat-panel-title> {{ cat.title }} </mat-panel-title>
-                <mat-panel-description>
-
-                </mat-panel-description>
+                <mat-panel-description> </mat-panel-description>
               </mat-expansion-panel-header>
-              <p>
-                @for(item of cat.content;track $index){
-                <p>
-                  <a [href]="item.link" target="_blank">
-                    {{ item.name }}
-                  </a>
-                </p>
-                }
-              </p>
+              <ng-container
+                *ngTemplateOutlet="listTemplate; context: { list: cat.links }"
+              ></ng-container>
             </mat-expansion-panel>
+          } @else {
+            <p>
+              <a [href]="cat?.link" target="_blank"> {{ cat.title }} </a>
+            </p>
           }
-        </mat-accordion> -->
-      </div>
-    </div>
+        }
+      </mat-accordion>
+    </ng-template>
   `,
 })
 export class TeachingAidComponent implements OnInit {
@@ -135,6 +139,124 @@ export class TeachingAidComponent implements OnInit {
       title: 'Balvikas Gita sloka',
       link: 'https://drive.google.com/drive/folders/1x11Vkc2Qe2LJnlaWhNAIOEkCcCHHI1ky?usp=drive_link',
       content: [],
+    },
+  ];
+  data_stotras = [
+    {
+      title: 'GROUP-1 1ST YEAR',
+      links: [
+        {
+          title: 'ଓଁକାରଂ ବିନ୍ଦୁ',
+          links: [
+            {
+              title: 'Audio (mp3)',
+              link: 'files/stotras/GROUP-1_1ST_YEAR/ଓଁକାରଂ%20ବିନ୍ଦୁ/OmKaramVindu.mp3',
+            },
+            {
+              title: 'Video',
+              link: 'files/stotras/GROUP-1_1ST_YEAR/ଓଁକାରଂ%20ବିନ୍ଦୁ/ଓଁ କାରଂ ବିନ୍ଦୁ ସଂଯୁକ୍ତଂ.mp4',
+            },
+            {
+              title: 'PDF',
+              link: 'files/stotras/GROUP-1_1ST_YEAR/ଓଁକାରଂ%20ବିନ୍ଦୁ/ଓଁ କାରଂ ବିନ୍ଦୁ...pdf',
+            },
+            {
+              title: 'PowerPoint',
+              link: 'files/stotras/GROUP-1_1ST_YEAR/ଓଁକାରଂ%20ବିନ୍ଦୁ/ଓଁ କାରଂ ବିନ୍ଦୁ...pptx',
+            },
+            {
+              title: 'Audio (acc)',
+              link: 'files/stotras/GROUP-1_1ST_YEAR/ଓଁକାରଂ%20ବିନ୍ଦୁ/ଓଁକାରଂ ସୂହ ଗାନ.aac',
+            },
+          ],
+        },
+        {
+          title: 'କର ଚରଣ କୃତଂବାକ',
+          links: [
+            {
+              title: 'Audio (mp3)',
+              link: 'files/stotras/GROUP-1_1ST_YEAR/କର ଚରଣ କୃତଂବାକ/karacharana.mp3',
+            },
+            {
+              title: 'Video',
+              link: 'files/stotras/GROUP-1_1ST_YEAR/କର ଚରଣ କୃତଂବାକ/କର ଚରଣ ଅଭିନୟ ଭିଡିଓ.mp4',
+            },
+            {
+              title: 'Audio Group (mp3)',
+              link: 'files/stotras/GROUP-1_1ST_YEAR/କର ଚରଣ କୃତଂବାକ/କର ଚରଣ ସମୂହ ଗାନ.mp3',
+            },
+            {
+              title: 'PDF',
+              link: 'files/stotras/GROUP-1_1ST_YEAR/କର ଚରଣ କୃତଂବାକ/କର ଚରଣ.pdf',
+            },
+            {
+              title: 'PowerPoint',
+              link: 'files/stotras/GROUP-1_1ST_YEAR/କର ଚରଣ କୃତଂବାକ/କର ଚରଣ.pptx',
+            },
+          ],
+        },
+        {
+          title: 'କରାଗ୍ରେ ବସତେ',
+          links: [
+            {
+              title: '',
+              link: '',
+            },
+          ],
+        },
+        {
+          title: 'ଗୁରୁର୍ବ୍ରହ୍ମା ଗୁରୁର୍ବିଷ୍ଣୁ',
+          links: [
+            {
+              title: '',
+              link: '',
+            },
+          ],
+        },
+        {
+          title: 'ତ୍ବମେବ ମାତା',
+          links: [
+            {
+              title: '',
+              link: '',
+            },
+          ],
+        },
+        {
+          title: 'ବକ୍ରତୁଣ୍ଡ ମହାକାୟ',
+          links: [
+            {
+              title: '',
+              link: '',
+            },
+          ],
+        },
+        {
+          title: 'ହରିର୍ଦାତା ହରିର୍ଭୋକ୍ତା',
+          links: [
+            {
+              title: '',
+              link: '',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      title: 'GROUP-1 2ND YEAR',
+      links: [
+        {
+          title: 'Sri Sathya Sai Stotras-1st Year',
+        },
+      ],
+    },
+    {
+      title: 'GROUP-1 3RD YEAR',
+      links: [
+        {
+          title: 'Sri Sathya Sai Stotras-1st Year',
+        },
+      ],
     },
   ];
 }
